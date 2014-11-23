bab4ed696bcfdfdf8755df53ed76c8e7b72de51b
salas <salas.106.212@gmail.com>
2014-11-23 23:36:40 +0100

    Adding encoding utf8 to yaml dumping and C module when available for improving perf.

--- utils/config.py
+++ utils/config.py
@@ -25,7 +25,7 @@ def get_config(config_path=None):
     if config_path is None:
         try:
             with open(config_file_path, 'rb') as config_stream:
-                config_dict = yaml.load(config_stream)
+                config_dict = yaml.load(config_stream, Loader=Loader)
         except IOError:
             logger.info('')
             try:
@@ -39,22 +39,22 @@ def get_config(config_path=None):
                 os.utime(config_file_path, None)
             try:
                 with open(config_example_path, 'rb') as config_example_stream:
-                    config_dict_example = yaml.load(config_example_stream)
+                    config_dict_example = yaml.load(config_example_stream, Loader=Loader)
                 # TODO : console based example file modification
                 with open(config_file_path, 'wb') as config_stream:
-                    yaml.dump(config_dict_example, config_stream)
+                    yaml.dump(config_dict_example, config_stream, Dumper=Dumper, encoding='utf-8')
             except IOError:
                 logger.critical("No example file. Exiting.")
                 sys.exit(0)
             try:
                 with open(config_file_path, 'rb') as config_stream:
-                    config_dict = yaml.load(config_stream)
-            except Exception:
+                    config_dict = yaml.load(config_stream, Loader=Loader)
+            except IOError:
                 sys.exit(0)
     else:
         try:
             with open(config_path, 'rb') as config_stream:
-                config_dict = yaml.load(config_stream)
+                config_dict = yaml.load(config_stream, Loader=Loader)
         except IOError as e:
             logger.exception(e)
             raise
