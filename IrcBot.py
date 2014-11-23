7f2b96b637629a5caa30bc5631d58aca7ae52515
salas <salas.106.212@gmail.com>
2014-11-23 12:54:47 +0100

    Debut bot

--- IrcBot.py
+++ IrcBot.py
@@ -5,3 +5,10 @@
     Just start it and it will read the configuration file (if existing) or create one step by step (if not).
 """
 
+import utils.config
+import utils.log
+
+logger = utils.log.get_logger('root')
+conf = utils.config.get_config()
+
+logger.debug("test")
--- utils/config.py
+++ utils/config.py
@@ -11,22 +11,21 @@ except ImportError:
 import utils.log
 logger = utils.log.get_logger('config')
 
-config_dir_path = os.path.abspath(os.path.join(os.sep, os.path.pardir(os.path.dirname(__file__)), 'config'))
-config_file_path = os.path.abspath(os.path.join(config_dir_path, 'config.yaml'))
-config_example_path = os.path.abspath(os.path.join(config_dir_path, 'config.example.yaml'))
+config_dir_path = os.path.abspath(os.path.join(os.sep, os.path.abspath(__file__), os.path.pardir, 'config'))
+config_file_path = os.path.join(config_dir_path, 'config.yaml')
+config_example_path = os.path.join(config_dir_path, 'config.example.yaml')
 logger.debug('The config file path is {}.'.format(config_file_path))
 
 
-def config():
+def get_config():
     try:
         with open(config_file_path, 'rb') as config_stream:
             config_dict = load(config_stream)
     except IOError:
         logger.info('')
-        # mkdir -p
         try:
             os.makedirs(config_dir_path)
-        except OSError as exc: # Python >2.5
+        except OSError as exc:
             if exc.errno == errno.EEXIST and os.path.isdir(config_dir_path):
                 pass
             else:
