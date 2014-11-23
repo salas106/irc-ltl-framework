ce6493a63529bd5d00c5abb4e8d1eacbcb86210a
salas <salas.106.212@gmail.com>
2014-11-23 19:28:07 +0100

    Nose test config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,11 @@
 from nose.tools import raises
+import utils.config
 
 
 @raises(IOError)
-def test_me():
-    with open('/test/me/not/') as test:
-        pass
+def config_wrong_path_raise_exception():
+    utils.config.get_config('./some/path/not/existing')
+
+
+def config_right_path_return_dict():
+    assert isinstance(utils.config.get_config(), dict)
--- utils/config.py
+++ utils/config.py
@@ -21,33 +21,41 @@ config_example_path = path.join(config_dir_path, 'config.example.yaml')
 logger.debug('The config file path is {}.'.format(config_file_path))
 
 
-def get_config():
-    try:
-        with open(config_file_path, 'rb') as config_stream:
-            config_dict = yaml.load(config_stream)
-    except IOError:
-        logger.info('')
-        try:
-            os.makedirs(config_dir_path)
-        except OSError as exc:
-            if exc.errno == errno.EEXIST and path.isdir(config_dir_path):
-                pass
-            else:
-                raise
-        with open(config_file_path, 'a'):
-            os.utime(config_file_path, None)
-        try:
-            with open(config_example_path, 'rb') as config_example_stream:
-                config_dict_example = yaml.load(config_example_stream)
-            # TODO : console based example file modification
-            with open(config_file_path, 'wb') as config_stream:
-                yaml.dump(config_dict_example, config_stream)
-        except IOError:
-            logger.critical("No example file. Exiting.")
-            sys.exit(0)
+def get_config(config_path=None):
+    if config_path is None:
         try:
             with open(config_file_path, 'rb') as config_stream:
                 config_dict = yaml.load(config_stream)
-        except Exception:
-            sys.exit(0)
+        except IOError:
+            logger.info('')
+            try:
+                os.makedirs(config_dir_path)
+            except OSError as exc:
+                if exc.errno == errno.EEXIST and path.isdir(config_dir_path):
+                    pass
+                else:
+                    raise
+            with open(config_file_path, 'a'):
+                os.utime(config_file_path, None)
+            try:
+                with open(config_example_path, 'rb') as config_example_stream:
+                    config_dict_example = yaml.load(config_example_stream)
+                # TODO : console based example file modification
+                with open(config_file_path, 'wb') as config_stream:
+                    yaml.dump(config_dict_example, config_stream)
+            except IOError:
+                logger.critical("No example file. Exiting.")
+                sys.exit(0)
+            try:
+                with open(config_file_path, 'rb') as config_stream:
+                    config_dict = yaml.load(config_stream)
+            except Exception:
+                sys.exit(0)
+    else:
+        try:
+            with open(config_path, 'rb') as config_stream:
+                config_dict = yaml.load(config_stream)
+        except IOError as e:
+            logger.exception()
+            raise
     return config_dict
